08a02cdeb857c330e177c1ae455721a4c3d5b217
Isaak Tsalicoglou <isaak@waseigo.com>
2025-09-15 22:02:33 +0300

    determine JSON type with `get_env/2` of `:db`; `mix format`

--- priv/repo/migrations/001_ex_double_entry_tables.exs
+++ priv/repo/migrations/001_ex_double_entry_tables.exs
@@ -3,40 +3,61 @@ defmodule ExDoubleEntry.Repo.Migrations.ExDoubleEntryMoney do
 
   def change do
     json_type =
-      if ExDoubleEntry.Repo.__adapter__ == Ecto.Adapters.Postgres do
-        :jsonb
-      else
-        :json
-      end
+      if Application.get_env(:ex_double_entry, :db) == :postgres,
+        do: :jsonb,
+        else: :json
 
-    create table(:"#{ExDoubleEntry.db_table_prefix}account_balances") do
-      add :identifier, :string, null: false
-      add :currency, :string, null: false
-      add :scope, :string, null: false, default: ""
-      add :balance_amount, :bigint, null: false
+    create table(:"#{ExDoubleEntry.db_table_prefix()}account_balances") do
+      add(:identifier, :string, null: false)
+      add(:currency, :string, null: false)
+      add(:scope, :string, null: false, default: "")
+      add(:balance_amount, :bigint, null: false)
 
       timestamps(type: :utc_datetime_usec)
     end
 
-    create index(:"#{ExDoubleEntry.db_table_prefix}account_balances", [:scope, :currency, :identifier], unique: true, name: :scope_currency_identifier_index)
+    create(
+      index(
+        :"#{ExDoubleEntry.db_table_prefix()}account_balances",
+        [:scope, :currency, :identifier],
+        unique: true,
+        name: :scope_currency_identifier_index
+      )
+    )
 
-    create table(:"#{ExDoubleEntry.db_table_prefix}lines") do
-      add :account_identifier, :string, null: false
-      add :account_scope, :string, null: false, default: ""
-      add :currency, :string, null: false
-      add :amount, :bigint, null: false
-      add :balance_amount, :bigint, null: false
-      add :code, :string, null: false
-      add :partner_identifier, :string, null: false
-      add :partner_scope, :string, null: false, default: ""
-      add :metadata, json_type
-      add :partner_line_id, references(:"#{ExDoubleEntry.db_table_prefix}lines")
-      add :account_balance_id, references(:"#{ExDoubleEntry.db_table_prefix}account_balances"), null: false
+    create table(:"#{ExDoubleEntry.db_table_prefix()}lines") do
+      add(:account_identifier, :string, null: false)
+      add(:account_scope, :string, null: false, default: "")
+      add(:currency, :string, null: false)
+      add(:amount, :bigint, null: false)
+      add(:balance_amount, :bigint, null: false)
+      add(:code, :string, null: false)
+      add(:partner_identifier, :string, null: false)
+      add(:partner_scope, :string, null: false, default: "")
+      add(:metadata, json_type)
+      add(:partner_line_id, references(:"#{ExDoubleEntry.db_table_prefix()}lines"))
+
+      add(:account_balance_id, references(:"#{ExDoubleEntry.db_table_prefix()}account_balances"),
+        null: false
+      )
 
       timestamps(type: :utc_datetime_usec)
     end
 
-    create index(:"#{ExDoubleEntry.db_table_prefix}lines", [:code, :account_identifier, :currency, :inserted_at], name: :code_account_identifier_currency_inserted_at_index)
-    create index(:"#{ExDoubleEntry.db_table_prefix}lines", [:account_scope, :account_identifier, :currency, :inserted_at], name: :account_scope_account_identifier_currency_inserted_at_index)
+    create(
+      index(
+        :"#{ExDoubleEntry.db_table_prefix()}lines",
+        [:code, :account_identifier, :currency, :inserted_at],
+        name: :code_account_identifier_currency_inserted_at_index
+      )
+    )
+
+    create(
+      index(
+        :"#{ExDoubleEntry.db_table_prefix()}lines",
+        [:account_scope, :account_identifier, :currency, :inserted_at],
+        name: :account_scope_account_identifier_currency_inserted_at_index
+      )
+    )
   end
 end
