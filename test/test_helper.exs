f3125228fac9ab87c3c0a18c96e8ac0f5a79d6a7
Isaak Tsalicoglou <isaak@waseigo.com>
2025-09-15 22:03:21 +0300

    exclude tests that require locking when using SQLite

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,5 +1,10 @@
 {:ok, _} = Application.ensure_all_started(:ex_machina)
 ExUnit.start(timeout: 300_000)
+
+if Application.get_env(:ex_double_entry, :db) == :sqlite3 do
+  ExUnit.configure(exclude: [:requires_locking])
+end
+
 Ecto.Adapters.SQL.Sandbox.mode(ExDoubleEntry.repo(), :manual)
 
 require Logger
