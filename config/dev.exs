b7eacafbe47415d88f7fd9aaeadfd52106492afc
Isaak Tsalicoglou <isaak@waseigo.com>
2025-09-15 21:57:01 +0300

    add commented-out configuration for SQLite

--- config/dev.exs
+++ config/dev.exs
@@ -6,3 +6,10 @@ config :ex_double_entry, ExDoubleEntry.Repo,
   database: "ex_double_entry_dev",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
+
+# When using SQLite
+# config :ex_double_entry, ExDoubleEntry.Repo,
+#   database: Path.expand("../ex_double_entry_dev.db", __DIR__),
+#   pool_size: 5,
+#   stacktrace: true,
+#   show_sensitive_data_on_connection_error: true
